ac44766134c8aae50320fbedb5da6cd650eb2705
Kisly93 <spsvk.kis.sov@gmail.com>
2023-03-10 20:45:59 +0100

    changed name variables

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -17,11 +17,11 @@ def fetch_epic_catalog(api_key, latest_epic_image_url):
     return response.json()
 
 
-def get_epic_img(api_key, epic_image_archive_url, format_date, epic_image):
+def get_epic_img(api_key, epic_image_archive_url, date_format, epic_image):
     payload = {
         'api_key': api_key
     }
-    response = requests.get(f'{epic_image_archive_url}/{format_date}/png/{epic_image}.png', params=payload)
+    response = requests.get(f'{epic_image_archive_url}/{date_format}/png/{epic_image}.png', params=payload)
     response.raise_for_status()
     return response.url
 
@@ -30,12 +30,12 @@ def cut_epic_img(url):
     parsed_url_nasa = urlparse(url)
     cropped_url_nasa = f"{parsed_url_nasa.path}"
     unqoute_url_nasa = unquote(cropped_url_nasa)
-    split_url_nasa = os.path.split(unqoute_url_nasa)
-    return split_url_nasa[1]
+    nasa_split_url = os.path.split(unqoute_url_nasa)
+    return nasa_split_url[1]
 
 
-def fetch_epic_photo(api_key, epic_image_archive_url, format_date, epic_image):
-    url = get_epic_img(api_key, epic_image_archive_url, format_date, epic_image)
+def fetch_epic_photo(api_key, epic_image_archive_url, date_format, epic_image):
+    url = get_epic_img(api_key, epic_image_archive_url, date_format, epic_image)
     response = requests.get(url)
     response.raise_for_status()
     with open(f'images/{cut_epic_img(url)}', 'wb') as file:
@@ -59,8 +59,8 @@ def main():
         epic_date = image_details['date']
         epic_image = image_details['image']
         old_date = datetime.datetime.strptime(epic_date, "%Y-%m-%d %H:%M:%S").date()
-        format_date = datetime.datetime.strftime(old_date, "%Y/%m/%d")
-        fetch_epic_photo(api_key, epic_image_archive_url, format_date, epic_image)
+        date_format = datetime.datetime.strftime(old_date, "%Y/%m/%d")
+        fetch_epic_photo(api_key, epic_image_archive_url, date_format, epic_image)
 
 
 if __name__ == '__main__':
